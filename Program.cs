892081cdd9fd160af911860406048a02bfd66e1c
gregory <wiznergregory@gmail.com>
2022-04-18 19:02:18 -0400

    Exercize2b

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿double userIn = 0;
+
+Console.WriteLine("Pick a number:");
+string userInput = Console.ReadLine();
+double.TryParse(userInput, out userIn);
+Console.WriteLine();
+Console.WriteLine(userIn + 1);
+Console.ReadLine();
